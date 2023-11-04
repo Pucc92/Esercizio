892ad8363b5666c9415656395b085ab2d577165b
Pucc92 <jopucc92@gmail.com>
2023-11-04 18:28:14 +0100

    rifatto esercizio pokedex da solo

--- Pokedex-da-solo/script.js
+++ Pokedex-da-solo/script.js
@@ -1,62 +1,57 @@
 const primaGen = 151;
 let pokedex = {};
 
-window.onload =  async function() {
+window.onload = async function () {
+  for (let i = 1; i < primaGen; i++) {
+    await prendiPokemon(i);
+    let pokemon = document.createElement("div");
+    pokemon.id = i;
+    pokemon.innerText = i.toString() + " . " + pokedex[i].nome.toUpperCase();
+    pokemon.classList.add("nome-pokemon");
+    pokemon.addEventListener("click", caricaPokemon);
+    document.querySelector(".lista-nomi").appendChild(pokemon);
+  }
+};
 
-    for (let i = 1; i < primaGen; i++) {
-        await ottieniPokemon(i);
-        
-        let pokemon = document.createElement('div');
-        pokemon.id = i;
-        pokemon.innerText = i.toString() + ' . ' + pokedex[i].nome.toUpperCase();
-        pokemon.classList.add('nome-pokemon');
-        document.querySelector('.lista-nomi').appendChild(pokemon);
-        pokemon.addEventListener('click', aggiornaPokemon);
-    }
-    
+async function prendiPokemon(num) {
+  let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+  let risposta = await fetch(url);
+  let pokemon = await risposta.json();
+  // console.log(pokemon);
 
+  let nomePokemon = pokemon.name;
+  let tipiPokemon = pokemon.types;
+
+  let imgPokemon = pokemon.sprites.front_default;
+
+  risposta = await fetch(pokemon.species.url);
+  let descPokemon = await risposta.json();
+
+  descPokemon = descPokemon.flavor_text_entries[9].flavor_text;
+  // console.log(descPokemon);
+
+  pokedex[num] = {
+    nome: nomePokemon,
+    img: imgPokemon,
+    tipi: tipiPokemon,
+    desc: descPokemon,
+  };
 }
 
-async function ottieniPokemon(num) {
-    let url = 'https://pokeapi.co/api/v2/pokemon/' + num.toString();
-    let risposta = await fetch(url);
-    let pokemon = await risposta.json();
-    // console.log(pokemon);
+function caricaPokemon() {
+  document.getElementById("pokemon-img").src = pokedex[this.id].img;
 
-    //Ottengo il nome del pokemon
-    let nomePokemon = pokemon.name;
-    // console.log(nomePokemon);
-    //Ottengo l' immagine del pokemon
-    let imgPokemon = pokemon.sprites.front_default;
-    // console.log(imgPokemon);
-    let tipoPokemon = pokemon.types;
-    // console.log(tipoPokemon);
-    //Ottengo descrizione pokemon
-    risposta = await fetch(pokemon.species.url);
-    let descPokemon = await risposta.json();
-    descPokemon = descPokemon.flavor_text_entries[9].flavor_text;
+  let parTipo = document.getElementById("pokemon-type");
+  while (parTipo.firstChild) {
+    parTipo.firstChild.remove();
+  }
+  let tipi = pokedex[this.id].tipi;
 
-    pokedex[num] = { 'nome': nomePokemon, 'img': imgPokemon, 'tipi': tipoPokemon, 'desc': descPokemon };
-    // console.log(pokedex);
-}
+  tipi.forEach((tipo) => {
+    let tipoBox = document.createElement("span");
+    tipoBox.innerText = tipo.type.name.toUpperCase();
+    parTipo.appendChild(tipoBox);
+  });
 
-function aggiornaPokemon() {
-    document.getElementById('pokemon-img').src = pokedex[this.id].img;
-
-    
-    //clear previous type
-    let divPokemon = document.getElementById("pokemon-type");
-    while (divPokemon.firstChild) {
-        divPokemon.firstChild.remove();
-    }
-
-    let types = pokedex[this.id].tipi;
-    console.log(types);
-    types.forEach(tipo => {
-        let boxTipi = document.createElement('span');
-        boxTipi.innerText = tipo.type.name.toUpperCase();
-        divPokemon.appendChild(boxTipi);
-    });
-
-    document.getElementById('pokemon-desc').innerText = pokedex[this.id].desc;
+  document.getElementById("pokemon-desc").innerText = pokedex[this.id].desc;
 }
