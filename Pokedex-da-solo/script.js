d026260d627ebf6e42893941e77ea5c89908a15c
Pucc92 <jopucc92@gmail.com>
2023-11-06 20:39:56 +0100

    rifatto pokedex da solo

--- Pokedex-da-solo/script.js
+++ Pokedex-da-solo/script.js
@@ -5,11 +5,11 @@ window.onload = async function () {
   for (let i = 1; i < primaGen; i++) {
     await prendiPokemon(i);
     let pokemon = document.createElement("div");
+    document.querySelector(".lista-nomi").appendChild(pokemon);
     pokemon.id = i;
     pokemon.innerText = i.toString() + " . " + pokedex[i].nome.toUpperCase();
     pokemon.classList.add("nome-pokemon");
     pokemon.addEventListener("click", caricaPokemon);
-    document.querySelector(".lista-nomi").appendChild(pokemon);
   }
 };
 
@@ -20,10 +20,8 @@ async function prendiPokemon(num) {
   // console.log(pokemon);
 
   let nomePokemon = pokemon.name;
-  let tipiPokemon = pokemon.types;
-
   let imgPokemon = pokemon.sprites.front_default;
-
+  let tipoPokemon = pokemon.types;
   risposta = await fetch(pokemon.species.url);
   let descPokemon = await risposta.json();
 
@@ -33,7 +31,7 @@ async function prendiPokemon(num) {
   pokedex[num] = {
     nome: nomePokemon,
     img: imgPokemon,
-    tipi: tipiPokemon,
+    tipi: tipoPokemon,
     desc: descPokemon,
   };
 }
@@ -41,16 +39,18 @@ async function prendiPokemon(num) {
 function caricaPokemon() {
   document.getElementById("pokemon-img").src = pokedex[this.id].img;
 
-  let parTipo = document.getElementById("pokemon-type");
-  while (parTipo.firstChild) {
-    parTipo.firstChild.remove();
-  }
-  let tipi = pokedex[this.id].tipi;
+  let contTipe = document.getElementById("pokemon-type");
 
+  while (contTipe.firstChild) {
+    contTipe.firstChild.remove();
+  }
+
+  let tipi = pokedex[this.id].tipi;
   tipi.forEach((tipo) => {
-    let tipoBox = document.createElement("span");
-    tipoBox.innerText = tipo.type.name.toUpperCase();
-    parTipo.appendChild(tipoBox);
+    let type = tipo.type.name;
+    let spanTipo = document.createElement("span");
+    spanTipo.innerText = type.toUpperCase();
+    contTipe.appendChild(spanTipo);
   });
 
   document.getElementById("pokemon-desc").innerText = pokedex[this.id].desc;
